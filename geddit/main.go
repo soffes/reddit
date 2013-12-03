4362c23cdfa084b52086d099cdb34b112bfcb801
Sam Soffes <sam@soff.es>
2013-12-03 13:47:56 -0800

    gofmt

--- geddit/main.go
+++ geddit/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "log"
-    "fmt"
-    "github.com/soffes/reddit"
+	"fmt"
+	"github.com/soffes/reddit"
+	"log"
 )
 
 func main() {
-    items, err := reddit.Get("golang")
-    if err != nil {
-        log.Fatal(err)
-    }
+	items, err := reddit.Get("golang")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    for _, item := range items {
-        fmt.Println(item)
-    }
+	for _, item := range items {
+		fmt.Println(item)
+	}
 }
--- reddit.go
+++ reddit.go
@@ -2,60 +2,60 @@
 package reddit
 
 import (
-    "net/http"
-    "fmt"
-    "encoding/json"
-    "errors"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
 )
 
 // Item describes a REddit item.
 type Item struct {
-    Title    string
-    URL      string
-    Comments int `json:"num_comments"`
+	Title    string
+	URL      string
+	Comments int `json:"num_comments"`
 }
 
 func (i Item) String() string {
-    com := ""
-    switch i.Comments {
-    case 0:
-        // Nothing
-    case 1:
-        com = " (1 comment)"
-    default:
-        com = fmt.Sprintf(" (%d comments)", i.Comments)
-    }
+	com := ""
+	switch i.Comments {
+	case 0:
+		// Nothing
+	case 1:
+		com = " (1 comment)"
+	default:
+		com = fmt.Sprintf(" (%d comments)", i.Comments)
+	}
 
-    return fmt.Sprintf("%s%s\n%s", i.Title, com, i.URL)
+	return fmt.Sprintf("%s%s\n%s", i.Title, com, i.URL)
 }
 
 type response struct {
-    Data struct {
-        Children []struct {
-            Data Item
-        }
-    }
+	Data struct {
+		Children []struct {
+			Data Item
+		}
+	}
 }
 
 // Get fetches the most recent Items posted to the specific subreddit.
 func Get(reddit string) ([]Item, error) {
-    url := fmt.Sprintf("http://reddit.com/r/%s.json", reddit)
-    resp, err := http.Get(url)
-    if err != nil {
-        return nil, err
-    }
-    defer resp.Body.Close()
-    if  resp.StatusCode != http.StatusOK {
-        return nil, errors.New(resp.Status)
-    }
-    r := new(response)
-    err = json.NewDecoder(resp.Body).Decode(r)
-    if err != nil {
-        return nil, err
-    }
-    items := make([]Item, len(r.Data.Children))
-    for i, child := range r.Data.Children {
-        items[i] = child.Data
-    }
-    return items, nil
+	url := fmt.Sprintf("http://reddit.com/r/%s.json", reddit)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+	r := new(response)
+	err = json.NewDecoder(resp.Body).Decode(r)
+	if err != nil {
+		return nil, err
+	}
+	items := make([]Item, len(r.Data.Children))
+	for i, child := range r.Data.Children {
+		items[i] = child.Data
+	}
+	return items, nil
 }
